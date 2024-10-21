8d2de0b6a34365e7866eb3208334959a69e0db4a
Raúl <raul.parada040103@gmail.com>
2024-10-21 10:48:20 +0200

    Hola mundo, escritura con > y cat y comentarios

---
+++ helloworld.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Hello World
+# echo "Hola Mundo!"
+
+# Este es un comentario
+cat >> hola.txt # Esto es otro comentario
+
+# Comentario de varias lineas
+: 'este es un comentario
+    de 2 lineas y puede
+    llegar a ser un
+    comentario mas largo'
